dde564ee6b37f696d4c1180b28f85e602ac76ff7
Chi290804 <chihoangf@gmail.com>
2024-11-21 17:47:33 +0700

    Elliptic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import rsa_backend as rsa_backend
+import elgammal_backend as elgammal_backend
+import elliptic_backend as elliptic_backend
  
 app = Flask(__name__)
 
@@ -83,6 +85,184 @@ def decrypt():
     
     decrypted_message = rsa_backend.decrypt_message(encrypted_message, private_key)
     return jsonify({'decrypted_message': decrypted_message})
+
+#@app.route('/tên API', methods=['POST'])
+@app.route('/generate_elgamal_keys', methods=['POST'])
+def generate_elgamal_keys():
+    try:
+        # Giá trị nhận từ HTML
+        data = request.json
+        
+        # Lấy giá trị p và a từ data
+        p = data.get('valueP')
+        a = data.get('a')
+
+        # Kiểm tra xem p và a có tồn tại và có thể chuyển sang số nguyên không
+        if p is None or not p.isdigit():
+            return jsonify({'error': 'p must be a valid integer'}), 400
+        if a is None or not a.isdigit():
+            return jsonify({'error': 'a must be a valid integer'}), 400
+
+        # Chuyển p và a sang kiểu int
+        p = int(p)
+        a = int(a)
+
+        # Kiểm tra số nguyên tố của p
+        if not elgammal_backend.is_prime(p):
+            return jsonify({'error': 'p must be a prime number'}), 400
+
+        # Sử dụng input để tạo khóa ElGamal
+        public_key, private_key = elgammal_backend.generate_elgamal_keys(p, a)
+
+        # Trả về kết quả cho client
+        return jsonify({
+            'public_key': public_key,
+            'private_key': private_key,
+        })
+
+    except Exception as e:
+        # Xử lý lỗi chung
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/hashing', methods=['POST'])
+def hashing():
+    data = request.json
+    message = data.get('message')
+    message_int = elgammal_backend.hashing(message)
+    return jsonify({'message_int': message_int})
+@app.route('/encrypt_elgamal', methods=['POST'])
+def encrypt_elgamal():
+    data = request.json
+    message = data.get('message').upper()
+    public_key = tuple(data.get('public_key'))
+    c1, c2 = elgammal_backend.encrypt(message, public_key)
+    return jsonify({'c1': c1, 'c2': c2})
+
+@app.route('/decrypt_elgamal', methods=['POST'])
+def decrypt_elgamal():
+    data = request.json
+    c1 = int(data.get('c1'))
+    c2 = int(data.get('c2'))
+    private_key = tuple(data.get('private_key'))
+    message = elgammal_backend.decrypt((c1, c2), private_key)
+    return jsonify({'message': message})
+
+#ELLIPTIC_CURVE
+@app.route('/check_condition', methods=['POST'])
+def check_condition_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    
+    is_valid = elliptic_backend.check_curve_conditions(a, b, p)
+
+    # Return the result with a "valid" key in the response
+    return jsonify({
+        "valid": is_valid,
+        "status": "success" if is_valid else "error",
+        "message": "Condition is valid." if is_valid else "Condition is not valid."
+    })
+@app.route('/random_G', methods=['POST'])
+def random_G_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    
+    point = elliptic_backend.get_random_point_on_curve(a, b, p)
+    return jsonify({"status": "success", "point": point})
+
+@app.route('/check_G', methods=['POST'])
+def check_G_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    # G là một tuple (x, y)
+    G = [data.get('x'), data.get('y')]
+    
+    if elliptic_backend.is_point_valid(a, b, p, G):
+        return jsonify({"status": True, "message": "Point belongs to the elliptic curve."})
+    else:
+        return jsonify({"status": False, "message": "Point does not belong to the elliptic curve."})
+
+@app.route('/caculate_order', methods=['POST'])
+def caculate_order_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    G = [data.get('x'), data.get('y')]
+    
+    order = elliptic_backend.calculate_order_of_point(a, b, p, G)
+    return jsonify({"order": order})
+
+# Tạo cặp khóa công khai và khóa bí mật
+@app.route('/generate_key', methods=['POST'])
+def generate_key_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    G = [data.get('x'), data.get('y')]  # Điểm cơ sở G
+    n = data.get('n')
+    s = data.get('s')
+    
+    private_key, public_key= elliptic_backend.generate_keys(a, b, p, G, n, s)
+    return jsonify({
+        "status": "success",
+        "public_key": public_key,
+        "private_key": private_key,
+    })
+
+@app.route('/message_to_point', methods=['POST'])
+def message_to_point_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    message = data.get('message')
+    x, y = elliptic_backend.map_message_to_curve(a, b, p, message)
+    return jsonify({"status": "success", "x": x, "y": y})
+
+# Mã hóa thông điệp
+@app.route('/encrypt-elip', methods=['POST'])
+def encrypt_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    message = data.get('message')
+    G = data.get('G')
+    n = data.get('n')
+    k = data.get('k')  # Số ngẫu nhiên k
+    
+    C1, C2 = elliptic_backend.encrypt_message(a, b, p, G, n, message, k)
+    return jsonify({
+        "status": "success",
+        "C1": C1,
+        "C2": C2
+    })
+
+# Giải mã thông điệp
+@app.route('/decrypt-elip', methods=['POST'])
+def decrypt_api():
+    data = request.get_json()
+    p = data.get('p')
+    a = data.get('a')
+    b = data.get('b')
+    G = data.get('G')
+    n = data.get('n')
+    s = data.get('s')
+    encrypted_message = (data.get('C1'), data.get('C2'))
+    
+    decrypted_message = elliptic_backend.decrypt_message(a, b, p, G, n, encrypted_message, s)
+    return jsonify({
+        "status": "success",
+        "decrypted_message": decrypted_message
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ elliptic_backend.py
@@ -0,0 +1,300 @@
+# %%
+import math
+def hashing(txt):
+    
+    # txt = txt.upper()
+    
+    ans = 0
+    
+    for c in txt:
+        ans = ans * 26 + (ord(c) - ord('A'))
+    
+    return ans
+
+def dehashing(num):
+    result = []
+    
+    while num > 0:
+        remainder = num % 26
+        char = chr(ord('A') + remainder)  # Chuyển đổi số thành ký tự
+        result.append(char)
+        num = num // 26
+    
+    # Đảo ngược chuỗi vì ta lấy phần dư từ cuối đến đầu
+    result.reverse()
+    return ''.join(result)
+# %%
+class EllipticCurve:
+    def __init__(self, a, b, p):
+        self.a = a
+        self.b = b
+        self.p = p
+        
+        if not self.check_condition(a, b, p):
+            raise ValueError("The curve is not valid")
+        
+    def check_condition(self, a, b, p):
+        return (4*a**3 + 27*b**2) % p != 0
+
+    def is_point_on_curve(self, point):
+        x, y = point
+        return (y**2) % self.p == (x**3 + self.a*x + self.b) % self.p
+
+    def add_points(self, p1, p2):
+        x1, y1 = p1
+        x2, y2 = p2
+        if p1 == (0, 0):
+            return p2
+        if p2 == (0, 0):
+            return p1
+        if x1 == x2 and y1 == -y2 % self.p:
+            return (0, 0)
+        if x1 == x2 and y1 == y2:
+            m = (3*x1**2 + self.a) * pow(2*y1, -1, self.p) % self.p
+        else:
+            m = (y2 - y1) * pow((x2 - x1) % self.p, -1, self.p) % self.p
+        x3 = (m**2 - x1 - x2) % self.p
+        y3 = (m*(x1 - x3) - y1) % self.p
+        return (x3, y3)
+
+    def multiply_point(self, point, n):
+        if n == 0:
+            return (0, 0)
+        if n == 1:
+            return point
+        if n % 2 == 0:
+            return self.multiply_point(self.add_points(point, point), n // 2)
+        return self.add_points(point, self.multiply_point(self.add_points(point, point), (n - 1) // 2))
+    
+    #calculate p1 - p2
+    def subtract_points(self, p1, p2):
+        x2, y2 = p2
+        return self.add_points(p1, (x2, -y2 % self.p))
+
+    def get_points(self):
+        points = []
+        for x in range(self.p):
+            y = self.get_y(x)
+            
+            if (y == None):
+                continue
+            
+            if (y[0] != 0):
+                points.append((x, y[0]))
+                points.append((x, y[1]))
+                
+            if len(points) > 500:
+                break
+            
+        return points
+    
+    def check_quadratic_residue(self, x):
+        return pow(x, (self.p - 1) // 2, self.p) == 1
+    
+    def get_y(self, x):
+        
+        a = (x**3 + self.a*x + self.b) % self.p
+        
+        if not self.check_quadratic_residue(a):
+            return None
+        
+        if (self.p % 4 == 3):
+            m = (self.p - 3) // 4
+            return pow(a, (m + 1), self.p), -pow(a, (m + 1), self.p) % self.p
+        else:
+            raise ValueError("Not implemented yet, p % 4 != 3")
+    
+    def get_order(self, point):
+        order = 1
+        while True:
+            if self.multiply_point(point, order) == (0, 0):
+                return order
+            order += 1
+
+
+# %%
+class EllipticCurveCryptography():
+    def __init__(self, curve, G, n):
+        # curve is an instance of EllipticCurve
+        self.curve = curve
+        # G is a point on the curve
+        self.G = G
+        # n is the order of the curve
+        self.n = curve.get_order(G)
+        
+        self.s = 1811
+        self.B = self.curve.multiply_point(self.G, self.s)
+        
+        
+    def get_B(self):
+        return self.B
+    def get_public_key(self):
+        return (self.curve.p, self.curve.a, self.curve.b, self.n, self.G, self.B)
+    
+    def get_private_key(self, s):
+        self.s = s
+        return self.s
+
+    def encrypt(self, M, k):
+        M1 = self.curve.multiply_point(self.G, k)
+        M2 = self.curve.add_points(M, self.curve.multiply_point(self.B, k))
+        return (M1, M2)
+
+    def decrypt(self, C1, C2, s):
+        return self.curve.subtract_points(C2, self.curve.multiply_point(C1, s))
+
+# %%
+import random
+
+class participant():
+    #M is the message to be signed, which is a point on the curve
+    def __init__(self, curve, G, n, M, signature):
+        self.ECC = EllipticCurveCryptography(curve, G, n)
+        
+        self.M = M
+        self.signature = signature
+    
+    def encrypt_message(self, other):
+        return other.ECC.encrypt(self.M)
+    
+    def decrypt_message(self, other, C1, C2):
+        return other.ECC.decrypt(C1, C2)
+    
+    def encrypt_signature(self, s):
+        n, G, B = self.ECC.get_public_key()[1:]
+        s = self.ECC.get_private_key(s)
+        
+        while(True):
+            k = random.randint(1, n - 1)
+            
+            M1 = self.ECC.curve.multiply_point(G, k)
+            
+            r = M1[0] % n
+            
+            if r == 0:
+                continue
+            
+            h = hashing(self.signature)
+            
+            u = (h + pow(s, r, n)) * pow(k, -1, n) % n
+            
+            if u == 0:
+                continue
+            
+            return (r, u)
+    
+    def decrypt_signature(self, cipher, s):
+        r, u = cipher
+        n, G, B = self.ECC.get_public_key()[1:]
+        s = self.ECC.get_private_key(s)
+        
+        w = pow(u, -1, n)
+        h = hashing(self.signature)
+        u1 = h * w % n
+        u2 = r * w % n
+        
+        P = self.ECC.curve.add_points(self.ECC.curve.multiply_point(G, u1), self.ECC.curve.multiply_point(B, u2))
+        
+        return True
+
+
+##
+# ĐỂ TẠO API
+##
+
+def check_curve_conditions(a, b, p):
+    try:
+        curve = EllipticCurve(a, b, p)
+        return True
+    except ValueError:
+        return False
+
+def get_points_on_curve(a, b, p):
+    curve = EllipticCurve(a, b, p)
+    return curve.get_points()
+
+def is_point_valid(a, b, p, point):
+    curve = EllipticCurve(a, b, p)
+    return curve.is_point_on_curve(point)
+
+def get_random_point_on_curve(a, b, p):
+    curve = EllipticCurve(a, b, p)
+    points = curve.get_points()
+    return random.choice(points) if points else None
+
+def calculate_order_of_point(a, b, p, G):
+    curve = EllipticCurve(a, b, p)
+    if curve.is_point_on_curve(G):
+        return curve.get_order(G)
+    return None
+
+def generate_keys(a, b, p, G, n, s):
+    curve = EllipticCurve(a, b, p)
+    ecc = EllipticCurveCryptography(curve, G, n)
+    private_key = ecc.get_private_key(s)
+    public_key = ecc.get_B()
+    # public_key = ecc.get_public_key()
+    return private_key, public_key
+
+def map_message_to_curve(a, b, p, message):
+    curve = EllipticCurve(a, b, p)
+    x = hashing(message)
+    while True:
+        try:
+            y = curve.get_y(x)
+            if y:
+                return (x, y[0])  # Lấy một nghiệm y
+        except ValueError:
+            pass
+        x += 1
+
+def encrypt_message(a, b, p, G, n, message, k):
+    curve = EllipticCurve(a, b, p)
+    ecc = EllipticCurveCryptography(curve, G, n)
+    M = map_message_to_curve(a, b, p, message)
+    return ecc.encrypt(M, k)
+
+def decrypt_message(a, b, p, G, n, encrypted_message, s):
+    curve = EllipticCurve(a, b, p)
+    ecc = EllipticCurveCryptography(curve, G, n)
+    C1, C2 = encrypted_message
+    decrypted_point = ecc.decrypt(C1, C2, s)
+    return decrypted_point
+
+# Thông số đường cong
+a, b, p = 1, 6, 11
+G = (2, 7)
+n = 5
+message = "HELLO"
+
+# 1. Kiểm tra điều kiện
+print("Điều kiện hợp lệ:", check_curve_conditions(a, b, p))
+
+# 2. Lấy điểm trên đường cong
+print("Các điểm trên đường cong:", get_points_on_curve(a, b, p))
+
+# 3. Kiểm tra điểm G
+print("G có thuộc đường cong không:", is_point_valid(a, b, p, G))
+
+# 4. Lấy điểm ngẫu nhiên
+random_point = get_random_point_on_curve(a, b, p)
+print("Điểm ngẫu nhiên trên đường cong:", random_point)
+
+# 5. Tính bậc của điểm G
+order = calculate_order_of_point(a, b, p, G)
+print("Bậc của G:", order)
+
+# 6. Tạo khóa
+private_key, public_key = generate_keys(a, b, p, G, n, s = 1811)
+print("Khóa bí mật:", private_key)
+print("Khóa công khai:", public_key)
+
+# 7. Mã hóa và giải mã tin nhắn
+poi = map_message_to_curve(a, b, p, message)
+print("M:", poi)
+
+encrypted_message = encrypt_message(a, b, p, G, n, message, k = 2)
+print("Tin nhắn đã mã hóa:", encrypted_message)
+
+decrypted_point = decrypt_message(a, b, p, G, n, encrypted_message, s = 1811)
+print("Tin nhắn đã giải mã:", decrypted_point)
